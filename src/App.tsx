70b08f843cbf8dabf92fcd43c3f8f8fbf20b96e0
Alexander Sulema <assulema@gmail.com>
2019-11-11 22:36:09 +0200

    started project, routing, header done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom';
+
 import './App.css';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+return (
+      <Router>
+        <div className="App">
+        <header>
+              <NavLink to="/"><img src="./images/logo.png" alt=""/></NavLink>
+          <div className="header_nav_options">
+            <nav>
+              <ul className="login">
+                <li><NavLink to="/strategy-providers">Strategy Providers</NavLink></li>
+                <li><NavLink to="/getting-started">Getting Started</NavLink></li>
+                <li><NavLink to="/simulator">Simulator</NavLink></li>
+                <li><NavLink to="/blog">Blog</NavLink></li>
+                <li><NavLink to="/about">About</NavLink></li>
+              </ul>
+            </nav>
+            <div className="enter_form">
+              <button className="login">Login</button>
+              <span>|</span>
+              <button className="sign_up">Sign Up</button>
+            </div>
+          </div>
+        </header>
+        <Switch>
+        </Switch>
+        </div>
+    </Router>
   );
 }
 
 export default App;
+          /*<Route exact path='/' component={props => <HomeComponent {...props} data={ this.state.cartItems } />} />*/
+          /*<Route path='/products/:product_id' component={props => <ProdComp {...props} data={ this.state.cartItems }  />} />*/

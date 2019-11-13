32606a710dd02694a49a2b745da691ee54b18095
Alexander Sulema <assulema@gmail.com>
2019-11-13 18:06:55 +0200

    Updates

---
+++ precache-manifest.c48b3cd192468046d947f2add1b2c97a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "373cc96670a0878ee6a85638ef45bf32",
+    "url": "/duplitrade-test/index.html"
+  },
+  {
+    "revision": "2a02c3f3c1fa00f0babf",
+    "url": "/duplitrade-test/static/css/main.a46e67ad.chunk.css"
+  },
+  {
+    "revision": "c3922b539b605a109427",
+    "url": "/duplitrade-test/static/js/2.8a3255a5.chunk.js"
+  },
+  {
+    "revision": "2a02c3f3c1fa00f0babf",
+    "url": "/duplitrade-test/static/js/main.50269f03.chunk.js"
+  },
+  {
+    "revision": "723dca8a73dbc5671c39",
+    "url": "/duplitrade-test/static/js/runtime-main.d216d059.js"
+  }
+]);
